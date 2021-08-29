c658405b93e4ebaf589a163e1417f84c7371dba5
Andes Lam <andeslam223@gmail.com>
2021-08-29 14:34:37 +0800

    added parameter binding attribute

---
+++ FYPAPI.Test/BindingAttribute/testParameterBinding.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Moq;
+using Xunit;
+using FYPAPI;
+using FYPAPI.Models;
+using System.Web.Http;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http.Controllers;
+using System.Threading;
+using FYPAPI.BindingAttribute;
+
+namespace FYPAPI.Test.BindingAttribute
+{
+    public class testParameterBinding
+    {
+        [Theory]
+        [InlineData("\"20201231094523\"")]
+        //[InlineData(null, false)]
+        public void testHasETag(string etagValue)
+        {
+            string parameterName = "etag";
+            //Arrange
+            HttpRequestMessage mockRequestMessage = new HttpRequestMessage(HttpMethod.Get, "https://localhost:44308/api/etag/1");
+            mockRequestMessage.Headers.IfNoneMatch.Add(new EntityTagHeaderValue(etagValue));
+
+            HttpActionContext mockHttpActionContext = new HttpActionContext
+            {
+                ControllerContext = new HttpControllerContext
+                {
+                    Request = mockRequestMessage
+                }
+            };
+
+            Mock<HttpParameterDescriptor> mockParameterDescriptor = new Mock<HttpParameterDescriptor>();
+            mockParameterDescriptor.SetupGet(i => i.ParameterName).Returns(parameterName);
+
+            ETagParameterBinding etagParameterBinding = new ETagParameterBinding(mockParameterDescriptor.Object);
+
+            //Act
+            etagParameterBinding.ExecuteBindingAsync(null, mockHttpActionContext, new CancellationTokenSource().Token).Wait();
+
+            //Assert
+            Assert.Equal(etagValue, mockHttpActionContext.ActionArguments[parameterName]);
+            /*Exception exception = Record.Exception(() => mockHttpActionContext.ActionArguments[parameterName]);
+            Assert.NotNull(exception);*/
+        }
+        [Theory]
+        [InlineData(null)]
+        public void testNoETag(string etagValue)
+        {
+            string parameterName = "etag";
+            //Arrange
+            HttpRequestMessage mockRequestMessage = new HttpRequestMessage(HttpMethod.Get, "https://localhost:44308/api/etag/1");
+
+            HttpActionContext mockHttpActionContext = new HttpActionContext
+            {
+                ControllerContext = new HttpControllerContext
+                {
+                    Request = mockRequestMessage
+                }
+            };
+
+            Mock<HttpParameterDescriptor> mockParameterDescriptor = new Mock<HttpParameterDescriptor>();
+            mockParameterDescriptor.SetupGet(i => i.ParameterName).Returns(parameterName);
+
+            ETagParameterBinding etagParameterBinding = new ETagParameterBinding(mockParameterDescriptor.Object);
+
+            //Act
+            etagParameterBinding.ExecuteBindingAsync(null, mockHttpActionContext, new CancellationTokenSource().Token).Wait();
+
+            //Assert
+            Exception exception = Record.Exception(() => mockHttpActionContext.ActionArguments[parameterName]);
+            Assert.NotNull(exception);
+        }
+    }
+}
---
+++ FYPAPI/BindingAttribute/ETagParameterBinding.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Metadata;
+
+namespace FYPAPI.BindingAttribute
+{
+    public class ETagParameterBinding : HttpParameterBinding
+    {
+        public ETagParameterBinding(HttpParameterDescriptor parameter) : base(parameter) { }
+        public override Task ExecuteBindingAsync(ModelMetadataProvider metadataProvider, HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            EntityTagHeaderValue etag = actionContext.Request.Headers.IfNoneMatch.FirstOrDefault();
+
+            if (etag != null)
+                actionContext.ActionArguments[Descriptor.ParameterName] = etag.Tag;
+
+            var tsc = new TaskCompletionSource<object>();
+            tsc.SetResult(null);
+            return tsc.Task;
+        }
+    }
+}
