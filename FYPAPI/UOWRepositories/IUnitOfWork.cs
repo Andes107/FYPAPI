2ae2e8dc72b17ba8475cde44e0a8defbe7ad2e0c
Andes Lam <andeslam223@gmail.com>
2021-08-29 15:15:23 +0800

    added uow interface

---
+++ FYPAPI/UOWRepositories/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FYPAPI.UOWRepositories
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        ICSEStudent CSEStudents { get; }
+        IFaculty Faculties { get; }
+        IFyp Fyps { get; }
+        IFYPCategory FYPCategories { get; }
+        IInterestedIn InterestedIns { get; }
+        IProjectGroup ProjectGroups { get; }
+        IRequirementGrade RequirementGrades { get; }
+        int Complete();
+    }
+}
