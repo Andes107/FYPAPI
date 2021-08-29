c239aa7f0aa2ab0b7d4662e9662cc51c8527ac25
Andes Lam <andeslam223@gmail.com>
2021-08-29 15:36:11 +0800

    tried to implement csestudent controller but to no avail

--- FYPAPI/Controllers/CSEStudentController.cs
+++ FYPAPI/Controllers/CSEStudentController.cs
@@ -5,12 +5,23 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Web.Http;
+using FYPAPI.BindingAttribute;
 using FYPAPI.Models;
+using FYPAPI.UOWRepositories;
 
 namespace FYPAPI.Controllers
 {
     [RoutePrefix("api/csestudent")]
     public class CSEStudentController: ApiController
     {
+        private readonly IUnitOfWork _context;
+        public CSEStudentController(IUnitOfWork context) { _context = context; }
+        [Route("getmany")]
+        public IHttpActionResult GetMany(CSEStudent student) { return Ok(_context.CSEStudents.FindMany(student)); }
+        [Route("getone")]
+        public HttpResponseMessage GetOne(string PK_tblCSEStudents, [IfNoneMatch]string etag)
+        {
+            CSEStudent stud = _context.CSEStudents.Get(PK_tblCSEStudents);
+        }
     }
 }
--- FYPAPI/UOWRepositories/IRepository.cs
+++ FYPAPI/UOWRepositories/IRepository.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Web;
 
 namespace FYPAPI.UOWRepositories
 {
     public interface IRepository<TEntity> where TEntity: class
     {
         IEnumerable<TEntity> GetAll();
-        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        IEnumerable<TEntity> FindMany(TEntity entity);
         void Add(TEntity entity);
     }
 }
