3e5ea382ec251b3d6648ab1409cdfaef8cac1e7c
Andes Lam <andeslam223@gmail.com>
2021-08-31 15:24:46 +0800

    successfully tested one with effort

--- FYPAPI.Infrastructure/FYPContext.cs
+++ FYPAPI.Infrastructure/FYPContext.cs
@@ -17,4 +17,88 @@ namespace FYPAPI.Infrastructure
     {
         public FYPContext(DbConnection dbConnection): base(dbConnection, contextOwnsConnection: true){}
     }
+    public partial class tblFaculty
+    {
+        public tblFaculty(string PK_tblFaculties, string name, string roomNo, string facultyCode)
+        {
+            this.PK_tblFaculties = PK_tblFaculties;
+            this.name = name;
+            this.roomNo = roomNo;
+            this.facultyCode = facultyCode;
+        }
+    }
+    public partial class tblFyp
+    {
+        public tblFyp(short PK_tblFyp, string title, string description, string category, 
+            string type, string otherRequirement, short minStudents, short maxStudents, 
+            bool isAvailable)
+        {
+            this.PK_tblFyp = PK_tblFyp;
+            /*
+                1. In the parameter list, I typed PK_tblFYp
+                    In the assignment, I used PK_tblFyp
+                    As a result, null is initialized
+                    Since PK_tblFyp is a primary key, it is not nullable
+                    The database complains accordingly
+             */
+            this.title = title;
+            this.description = description;
+            this.category = category;
+            this.type = type;
+            this.otherRequirements = otherRequirement;
+            this.minStudents = minStudents;
+            this.maxStudents = maxStudents;
+            this.isAvailable = isAvailable;
+        }
+    }
+    public partial class tblProjectGroup
+    {
+        public tblProjectGroup(short PK_tblProjectGroup, string groupCode, short? assignedFYP, string reader)
+        {
+            this.PK_tblProjectGroups = PK_tblProjectGroup;
+            this.groupCode = groupCode;
+            this.assignedFYP = assignedFYP;
+            this.reader = reader;
+        }
+    }
+    public partial class tblSupervis
+    {
+        public tblSupervis(string username, short fypId)
+        {
+            this.username = username;
+            this.fypId = fypId;
+        }
+    }
+    public partial class tblRequirementGrade
+    {
+        public tblRequirementGrade(string facultyUsername, string studentUsername, 
+            decimal? proposalReport, decimal? progressReport, decimal? finalReport,
+            decimal? presentation)
+        {
+            this.facultyUsername = facultyUsername;
+            this.studentUsername = studentUsername;
+            this.proposalReport = proposalReport;
+            this.progressReport = progressReport;
+            this.finalReport = finalReport;
+            this.presentation = presentation;
+        }
+    }
+    public partial class tblCSEStudent
+    {
+        public tblCSEStudent(string PK_tblCSEStudents, string name, short? groupId)
+        {
+            this.PK_tblCSEStudents = PK_tblCSEStudents;
+            this.name = name;
+            this.groupId = groupId;
+        }
+    }
+    public partial class tblInterestedIn
+    {
+        public tblInterestedIn(short fypId, short groupId, short priority)
+        {
+            this.fypId = fypId;
+            this.groupId = groupId;
+            this.priority = priority;
+        }
+    }
 }
--- FYPAPI.Test/Repositories/testRepository.cs
+++ FYPAPI.Test/Repositories/testRepository.cs
@@ -9,23 +9,24 @@ using System.Data.Entity.Core.EntityClient;
 
 namespace FYPAPI.Test.Repositories
 {
+    public class repositoryFixture
+    {
+        public repositoryFixture()
+        {
+            Effort.Provider.EffortProviderConfiguration.RegisterProvider();
+        }
+    }
     public class testRepository: IClassFixture<repositoryFixture>
     {
-        public testRepository() { EffortProviderFactory.ResetDb(); }
-
         [Fact]
         public void testContext()
         {
             EntityConnection connection = Effort.EntityConnectionFactory.CreatePersistent("name=FYPContext");
             FYPContext _context = new FYPContext(connection);
+            FYPContextInitializer.ContextInit(_context);
             List<tblFypCategory> students = new List<tblFypCategory>();
             using (_context)
             {
-                _context.tblFypCategories.Add(new tblFypCategory()
-                {
-                    PK_tblFypCategories = "AI"
-                });
-                _context.SaveChanges();
                 students = _context.tblFypCategories.ToList();
             }
             Assert.Equal(2, students.Count);
