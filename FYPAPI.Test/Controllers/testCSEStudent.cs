4c82b99ea6da4a8e46890111febba5bff271adc4
Andes Lam <andeslam223@gmail.com>
2021-09-07 10:00:15 +0800

    moq callback for ref type, delegate init

---
+++ FYPAPI.Test/Controllers/testCSEStudent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+using Moq;
+using FYPAPI.UOWRepositories;
+using FYPAPI.Models;
+
+namespace FYPAPI.Test.Controllers
+{
+    public class testCSEStudent
+    {
+        public delegate void delegateForRefString(string first, string second, ref string input);
+        [Fact]
+        public void testCSEStudentController()
+        {
+            //Assert
+            Mock<ICSEStudent> mockICSEStud = new Mock<ICSEStudent>();
+            mockICSEStud.Setup(call => call.Get(It.IsAny<string>(), It.IsAny<string>(), ref It.Ref<string>.IsAny))
+                        .Callback(new delegateForRefString((string first, string second, ref string input) => input = "!"))
+                        .Returns((CSEStudent)null);
+            Mock<IUnitOfWork> mockIUnit = new Mock<IUnitOfWork>();
+            mockIUnit.SetupGet(unit => unit.CSEStudents).Returns(mockICSEStud.Object);
+        }
+    }
+}
